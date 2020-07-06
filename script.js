2c9422af89778dfc62cd763aa094a344b01acec2
Matt Bongiorno <bigiorno@gmail.com>
2020-07-06 12:46:27 -0400

    generate button, parameter elements

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Select generate button
+const generateBtn = document.querySelector("#generate");
+
+//Select password parameters elements
+const lengthBox = document.querySelector('#length');
+const lowerBox = document.querySelector('#lowercase');
+const upperBox = document.querySelector('#uppercase');
+const numbersBox = document.querySelector('#numbers');
+const specialCharBox = document.querySelector('#special-char');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
