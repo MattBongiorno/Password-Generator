891edcd5152a627374a653fc57c1786da43fcaf4
Matt Bongiorno <bigiorno@gmail.com>
2020-07-06 21:28:10 -0400

    validade, return

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Select generate button
-const generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector('#generate');
 
 //Select password parameters elements
 const lengthBox = document.querySelector('#length');
@@ -10,41 +10,64 @@ const specialCharBox = document.querySelector('#special-char');
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-
+    let password = generatePassword();
+    let passwordText = document.querySelector('#password');
     passwordText.value = password;
-
 }
-// Password parameter element arrays
-const lowerLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
-const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-const numbers = '0123456789'.split('');
-const specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split('');
+// Logic to randomly generate password
+function generatePassword() {
 
-// Initialize password character array
-let passChoices = [];
+    // Password parameter element arrays
+    const lowerLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const upperLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+    const numbers = '0123456789'.split('');
+    const specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split('');
 
-// Initialize array for generated password
-let passBuilder = [];
+    // Initialize password character array
+    let passChoices = [];
 
-// Obtain length from form
-let pswdLength = parseInt(lengthBox.value);
+    // Initialize array for generated password
+    let passBuilder = [];
 
-// Add password characters to password array
-if (lowerBox.checked) {
-    passChoices = passChoices.concat(lowerLetters);
+    // Obtain length from form
+    let pswdLength = parseInt(lengthBox.value);
+
+    // Add password characters to password array
+    if (lowerBox.checked) {
+        passChoices = passChoices.concat(lowerLetters);
+    }
+    if (upperBox.checked) {
+        passChoices = passChoices.concat(upperLetters);
+    }
+    if (numbersBox.checked) {
+        passChoices = passChoices.concat(numbers);
+    }
+    if (specialCharBox.checked) {
+        passChoices = passChoices.concat(specialChars);
+    }
+
+    // Validate that a length of appropriate size was input
+    if (passChoices.length) {
+        if (pswdLength < 8 || pswdLength > 128 || isNaN(pswdLength)) {
+            alert('Please enter a password length between 8 and 128 characters.');
+            return '';
+        } else {
+            // Choose random characters to add to password
+            for (let i = 0; i < pswdLength; i++) {
+                let randChar = Math.floor(Math.random() * passChoices.length);
+                passBuilder.push(passChoices[randChar]);
+            }
+
+            // Create string of final password and return it
+            let password = passBuilder.join('');
+            return password;
+        }
+    } else {
+        // Make sure at least one parameter is checked
+        alert('Please check at least one character type.');
+        return '';
+    }
 }
-if (upperBox.checked) {
-    passChoices = passChoices.concat(upperLetters);
-}
-if (numbersBox.checked) {
-    passChoices = passChoices.concat(numbers);
-}
-if (specialCharBox.checked) {
-    passChoices = passChoices.concat(specialChars);
-}
-
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
